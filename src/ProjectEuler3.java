1a47f4166769d034617cfe607ee7d170f67eb6b5
sergisalv <sergisalv@gmail.com>
2024-08-28 14:00:07 +0200

    Ejercicio 3 resuelto

---
+++ src/ProjectEuler3.java
@@ -0,0 +1,29 @@
+public class ProjectEuler3 {
+    public static void main(String[] args) {
+    largestPrimeFactor();
+    }
+
+    public static void largestPrimeFactor(){
+
+        //Número que vamos a factorizar
+        Long num = 600851475143L;
+        //Numero primo
+        int primeNum;
+
+
+        // Dividimos entre 2 y los sucesivos número
+        for (primeNum = 2; primeNum < num ; primeNum++ ){
+
+            //Dividimos el número entre el que nos de como resultado 0 (Puesto que solo es divisible por ese número y por 1)
+            while (num % primeNum == 0){
+                num /= primeNum;
+            }
+        }
+
+        System.out.println("El mayor número primo es: " + primeNum);
+
+
+
+    }
+
+}
